4823fa2e8309963e2872bd7c355c89c2ef2afe2f
Jake McMillan <jakewmcmillan@gmail.com>
2022-08-24 17:45:10 -0400

    Defining product tags columns

--- models/Product.js
+++ models/Product.js
@@ -3,6 +3,7 @@ const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 const Category = require('./Category');
+const Tag = require('./Tag');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -39,10 +40,10 @@ Product.init(
     category_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'category',
+        model: Category,
         key: 'id',
       }
-    }
+    },
   },
   {
     sequelize,
