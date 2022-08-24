944a5bf287dade8f8518a58a24ebb184bab95e97
Jake McMillan <jakewmcmillan@gmail.com>
2022-08-24 17:05:33 -0400

    Set up fields and rules for product model

--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,7 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const Category = require('./Category');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -20,10 +21,17 @@ Product.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      defaultValue: "10",
+      defaultValue: 10,
       validate: {
         isNumeric: true
       }
@@ -31,7 +39,8 @@ Product.init(
     category_id: {
       type: DataTypes.INTEGER,
       references: {
-        
+        model: 'category',
+        key: 'id',
       }
     }
   },
